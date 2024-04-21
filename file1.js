e647e949839cccf2b17c4ec092dc68cc1c13a8f0
“Radhika Kedia” <radhikakedia249@gmail.com>
2024-04-21 18:36:50 +0530

    I learnt about strings

---
+++ file1.js
@@ -0,0 +1,17 @@
+let text1 = "radhika";
+let text2 = "kedia";
+let result = `         My name is ${text1} and my title is ${text2}               `;
+console.log(result);
+let result1 = result.trim();
+console.log(result1);
+let result2 = result1.toUpperCase();
+console.log(result2);
+let result3 = result2.toLowerCase();
+console.log(result3);
+let result4 = result3.slice(0,18);
+console.log(result4);
+console.log(typeof result4);
+let mystring = "20";
+mystring = +"20";
+console.log(typeof mystring);
+console.log(typeof Number(mystring));
