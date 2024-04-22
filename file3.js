a490d3b692dcd4f7c416da4dea67f8d945244fcc
“Radhika Kedia” <radhikakedia249@gmail.com>
2024-04-22 20:47:09 +0530

    truthy and falsy values

---
+++ file3.js
@@ -0,0 +1,21 @@
+// truthy and falsy values
+
+//falsy values
+let firstname = "radhika";
+if(firstname){
+    console.log(firstname);
+}else{
+    console.log(" Firstname is kinda empty");
+}
+
+
+//truthy
+//"abc"
+//1  , -1
+
+
+//falsy values
+//""
+//null
+//undefined
+//0
