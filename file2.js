e26ec534dd48190f993281d561c499952378a082
“Radhika Kedia” <radhikakedia249@gmail.com>
2024-04-22 19:55:28 +0530

    learnt boolean and comparison operator

---
+++ file2.js
@@ -0,0 +1,15 @@
+//boolean and comparison operator 
+let num1 = 5;
+let num2 = 7;
+let num3 = 7;
+let num4 = "7";
+
+console.log(num1<num2);
+
+//== / ===
+console.log(num2==num3); // it checks only the value
+console.log(num3===num4); //it checks the value as well as the datatype
+
+// != / !==
+console.log(num1 != num2);
+console.log(num2 !== num3);
