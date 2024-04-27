dea4a0911fbd8c68cedd88d488de2d82e1d2af87
“Radhika Kedia” <radhikakedia249@gmail.com>
2024-04-27 23:38:45 +0530

    for of loop

---
+++ file16.js
@@ -0,0 +1,20 @@
+// for of loop
+
+const girls = ['neha','hazel','naina','radhika'];
+const girls2 = [];
+
+for ( let girl of girls){
+    girls2.push(girl.toUpperCase());
+}
+console.log(girls2);
+
+
+
+
+const fruits = ['apple','grapes','mango'];
+const fruits2 = [];
+
+for (let fruit of fruits){
+    fruits2.push(fruit.toUpperCase());
+}
+console.log(fruits2);
