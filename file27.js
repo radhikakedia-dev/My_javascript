fa716cbbaddd25e620e97d851685a07cdcfa8e4b
“Radhika Kedia” <radhikakedia249@gmail.com>
2024-06-30 16:27:49 +0530

    Hoisting

---
+++ file27.js
@@ -0,0 +1,10 @@
+//hoisting
+
+
+hello();
+
+function hello(){
+    console.log("Hello Radhika");
+}
+
+// in case of function declaration we can call the function before creating them but we can't do the same in case of function expression and arrow function
