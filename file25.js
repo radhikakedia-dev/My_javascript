5c565abd2b45c9bc61e9536792bc61a4e9afccd6
“Radhika Kedia” <radhikakedia249@gmail.com>
2024-06-30 15:35:12 +0530

    function expression

---
+++ file25.js
@@ -0,0 +1,8 @@
+//  2nd way of function declaration
+//function expression
+//when you declare a function inside a variable it is known as function expression
+
+const singHappyBirthday = function(){
+    console.log("Happy Birthday to You");
+}
+console.log(singHappyBirthday());
